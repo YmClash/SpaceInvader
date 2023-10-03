571b9aa24dced6b20060e4e1bb192af788e76d21
YmClash <momo.toure27@gmail.com>
2023-10-03 16:47:04 +0200

    update

--- test_room.py
+++ test_room.py
@@ -1,25 +1,37 @@
-import random
-import pygame
-from snake import redimension
+# import random
+# import pygame
+# from snake import redimension
+#
+def get_string_methods():
+    i: int = 0
+    for method in dir(str):
+        if '__' not in method:
+            # print(f"{i} : {method}")
+            i += 1
+            print(f"{i} : {method}")
 
 
-pygame.init()
-
-fenetre = pygame.display.set_mode((200,200))
-
-img= pygame.image.load('mogwai .PNG')
-player = redimension(img)
-
-fenetre.blit(player,(0,0))
-
-pygame.display.flip()
-
-run = True
-
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+get_string_methods()
 
 
-pygame.quit()
+
+# pygame.init()
+#
+# fenetre = pygame.display.set_mode((200,200))
+#
+# img= pygame.image.load('mogwai .PNG')
+# player = redimension(img)
+#
+# fenetre.blit(player,(0,0))
+#
+# pygame.display.flip()
+#
+# run = True
+#
+# while run:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             run = False
+#
+#
+# pygame.quit()
