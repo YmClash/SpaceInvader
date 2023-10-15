9e548c9d3104e6042b15d4ac2860704f9e9fa814
YmClash <momo.toure27@gmail.com>
2023-10-15 19:22:17 +0200

    genetic algorithme

--- gen.py
+++ gen.py
@@ -1,27 +1,44 @@
 import random
+from icecream import ic
+
+MAX_GENERATION = 100
+
+# Definition initial
 
 
-popu = 2
-people = [random.sample(range(10),5) for i in range(popu)]
+popusize = 10
+people = [random.sample(range(10), 5) for i in range(popusize)]
 
 print(people)
 
+
 def inialise():
+    popusize = 10
+    people = [random.sample(range(10), 5) for i in range(popusize)]
+
+#Fonction d'evaluation
+def evalu(individual):
+    return sum(individual)
+
+
+def mutation(individual):
+    gene_mutant = random.randint(0,len(individual)-1)    #on choisis  un gene aleatoire a muter chez un individu
+    individual[gene_mutant]=random.randint(0,9)     # on  remplace le gene choix  par  une nouvelle choisi aussi aleatoirement
+    return individual                                         # return la nouvelle valeur muter
+def selection_parent() :
     pass
 
-def evalu():
-    pass
 
-def selection_parent():
-    pass
+def crossover(papa,mama):
+    cross = random.randint(1,len(papa)-1)       # on choisis un point de croisement aleatoire
+    child1 = papa[:cross] + mama[cross:]        # on cree un enfant 1 avec le gene de papa jusqua  la croisement et le gene de maman apres le croisement
+    child2 = papa[:cross] + mama[cross:]        # on cree un enfant 2 avec le gene de papa jusqua  la croisement et le gene de maman apres le croisement
+    return child1,child2
 
-def crossover():
-    pass
 
-def mutation():
-    pass
-def ancestre_evalue():
-    pass
+def ancestre_evalue(ancester ):
+    return sum(ancester)
 
-def remplacement():
+
+def remplacement() :
     pass
