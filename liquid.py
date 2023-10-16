e0f3b42fd2bb5a46bef299454e0d00baa97d7161
YmClash <momo.toure27@gmail.com>
2023-10-16 21:35:01 +0200

    Add Liquid Neuronal Network

--- liquid.py
+++ liquid.py
@@ -13,12 +13,12 @@ class LiquidNeuralNetwork(nn.Module):
 
         #creation des couches
         # self.output_layer = nn.Linear(self.hidden_size, self.output_size)
-        self.hidden_layers = nn.ModuleList([nn.Linear(self.input_size, self.hidden_size) for _ in range(5)])
+        self.hidden_layers = nn.ModuleList([nn.Linear(self.input_size if i == 0 else self.hidden_size, self.hidden_size) for i in range(5)])
+
         self.output_layer = nn.Linear(self.hidden_size,self.output_size)
 
     def forward(self,x):
-        x = torch.relu(self.in
-
+        # x = torch.relu(self.input_layer(x))
         for hidden_layer in self.hidden_layers:
             x = torch.relu(hidden_layer(x))
 
@@ -34,7 +34,7 @@ output_size = 5
 lucie = LiquidNeuralNetwork(input_size,hidden_size,output_size)
 
 input_data = (torch.randn(1, input_size))
-
+print("Input DATA:")
 print(input_data)
 
 output = lucie(input_data)
