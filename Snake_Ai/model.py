5b733cb80e5c5fc163814729ae35f33a02276e3c
YmClash <momo.toure27@gmail.com>
2024-01-04 00:29:30 +0100

    snake game

---
+++ Snake_Ai/model.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import os
+
+
+class QNET_Neuronal(nn.Module):
+    def __init__(self,input_size,hidden_size,output_size):
+        super().__init__()
+        self.qnet1 = nn.Linear(input_size,hidden_size)
+        self.qnet2 = nn.linear(hidden_size,output_size)
+
+
+    def forward(self,x):
+        x = F.relu(self.qnet1(x))
+        x = self.qnet2(x)
+        return x
+
+
+    def save(self,file_name='Snake_Model.pth'):
+        model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+
+        file_name = os.path.join(model_folder_path,file_name)
+        torch.save(self.state_dict(),file_name)
+
+
+class Qtrainer:
+    def __init__(self, model, lr , gamma):
+        self.model = model
+        self.lr = lr
+        self.gamma = gamma
+        self.optimizer = optim.Adam(model.parameters(),lr=self.lr)
+        self.criterion = nn.MSELoss()
+
+
+    def train_step(self,state,action,reward,next_state,done):
+        state = torch.tensor(state,dtype=torch.float)
+        next_state = torch.tensor(next_state,dtype=torch.float)
+        action = torch.unsqueeze(action,0)
+        reward = torch.unsqueeze(reward,0)
+
+        if len(state.shape) == 1:
+            state = torch.unsqueeze(state,0)
+            next_state = torch.unsqueeze(next_state,0)
+            action = torch.unsqueeze(action,0)
+            reward = torch.unsqueeze(reward,0)
+            done = (done,)
+
+            #### 1  prediction  de la valuer Q
+
+        predic = self.model(state)
+        target = predic.clone
+        for idx in range(len(done)):
+            Q_new = reward[idx]
+            if not done[idx]:
+                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+
+            target[idx][torch.argmax(action[idx]).item()] = Q_new
+
+        self.optimizer.zero_grad()
+        loss = self.criterion(target,predic)
+        loss.backward()
+        self.optimizer.step()
+
+
+
+
+
+
