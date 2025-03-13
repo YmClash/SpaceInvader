a7f9afbace2b3114ef1d1e21556062da24f6d4be
y_mc <momo.toure27@gmail.com>
2025-03-13 22:03:35 +0100

    adding asset

---
+++ GameBoy/carte/house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="house" tilewidth="16" tileheight="16" tilecount="1218" columns="42">
+ <image source="../Pygamon/assets/houses/house.png" trans="ffffff" width="674" height="473"/>
+</tileset>
