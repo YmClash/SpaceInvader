da65111afcdb0fd6879d250281fe320e8f76f9b7
y_mc <momo.toure27@gmail.com>
2025-05-30 15:24:18 +0200

    adding random  color

--- Damier/constants.py
+++ Damier/constants.py
@@ -1,9 +1,20 @@
 # Constantes
 
+
+import random
+
 TAILLE_FENETRE = 800
-TAILLE_CASE = TAILLE_FENETRE
-NOIR = (0, 0, 0)
+TAILLE_CASE = TAILLE_FENETRE // 8
+NOIR = [0, 0, 0]
 BLANC = (255, 255, 255)
 ROUGE = (255, 0, 0)
 MARRON = (139, 69, 19)
-BEIGE = (245, 245, 220)
+
+BEIGE = (245, 222, 179)
+
+
+RANDOM_COLORS_1 = [random.randint(0, 255) for i in range(3)]
+RANDOM_COLORS_2 = [random.randint(0, 255) for i in range(3)]
+
+
+
