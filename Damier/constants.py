7a4066f57efe995762e6858088c947bdafa6e780
y_mc <momo.toure27@gmail.com>
2025-05-31 17:38:18 +0200

    upgrade

--- Damier/constants.py
+++ Damier/constants.py
@@ -7,7 +7,7 @@ TAILLE_FENETRE = 800
 TAILLE_CASE = TAILLE_FENETRE // 8
 
 
-# NOIR = [0, 0, 0]
+
 
 ROUGE = (255, 0, 0)
 MARRON = (139, 69, 19)
@@ -21,11 +21,11 @@ RANDOM_COLORS_1 = [random.randint(0, 255) for i in range(3)]
 RANDOM_COLORS_2 = DOM
 RANDOM_COLORS_3 = [random.randint(0, 255) for i in range(3)]
 
+NOIR = [0, 0, 0]
+# NOIR = RANDOM_COLORS_3
 
-NOIR = RANDOM_COLORS_3
-
-# BLANC = (255, 255, 255)
-BLANC = RANDOM_COLORS_1  # Utilisation de la couleur aléatoire pour le blanc
+BLANC = (255, 255, 255)
+# BLANC = RANDOM_COLORS_1  # Utilisation de la couleur aléatoire pour le blanc
 
 
 
